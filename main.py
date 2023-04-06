16bb11e194bad4f541c28c34a06c738c18154813
Stimul88 <Stimul881988@mail.ru>
2023-04-06 23:10:51 +0300

    Corrected commit

--- main.py
+++ main.py
@@ -1,51 +1,73 @@
 import csv
 import re
 
-with open('phonebook_raw.csv', encoding='utf-8') as f:
+with open('phonebook_raw.csv', encoding='utf-8', newline='') as f:
     rows = csv.reader(f, delimiter=',')
     contacts_list = list(rows)
 
-dict_ = {}
 
-for i in contacts_list:
-    j = ', '.join(i)
-    pattern1 = r'(8|\+7)\s*[\s|\(]*(\d{3})\)*[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*\(*([доб.]*)\s*(\d*)\)*'
-    subst1 = r'+7(\2)\3-\4-\5 \6\7'
-    result1 = re.sub(pattern1, subst1, j)
-    pattern = r'^(\w+)[\s|\,]*(\w+)\,*\s*(\w*)'
-    subst = r'\1, \2, \3'
-    result = re.sub(pattern, subst, result1)
-    sp = result.split()
-    unific = ' '.join(sp[0:2])
-    unific1 = ' '.join(sp[2:])
+def reg():
+    dict_ = {}
+    dict1_ = {}
+    for i in contacts_list:
+        j = ','.join(i)
+        pattern = r'(8|\+7)\s*[\s|\(]*(\d{3})\)*[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*\(*([доб.]*)\s*(\d*)\)*'
+        subst = r'+7(\2)\3-\4-\5 \6\7'
+        result = re.sub(pattern, subst, j)
+        pattern1 = r'^(\w+)[\s|\,]*(\w+)\s?\,?(\w*)\,\,*(\,*)'
+        subst1 = r'\1,\2,\3,'
+        result1 = re.sub(pattern1, subst1, result)
+        pattern2 = r'Иван(\,,)'
+        subst2 = r'Иван,,,,,'
+        result2 = re.sub(pattern2, subst2, result1)
+        sp = result2.split(',')
+        unific_ = ','.join(sp[0:2])
+        unific1_ = ','.join(sp[2:])
+        if unific_ not in dict_.keys():
+            dict_[unific_] = unific1_.split(',')
+        else:
+            dict1_[unific_] = unific1_.split(',')
+    return dict_, dict1_
 
-    if unific not in dict_.keys():
-        dict_[unific] = [unific1]
-    else:
-        dict_[unific] += [unific1]
 
-list1_ = []
-counter = 0
-for i in dict_.values():
-    for j in i:
-        counter += 1
-        if counter >= 1:
-            list1_.append(j)
+def dict_add():
+    dict3_ = {}
+    for m, o in reg()[1].items():
+        for i, j in reg()[0].items():
+            if i == m:
+                list3_ = []
+                for p in range(len(j)):
+                    if j[p] == '':
+                        list3_.append(o[p])
+                        # dict3_[i] = j
+                    else:
+                        list3_.append(j[p])
+                        # dict3_[i] = o
+                dict3_[m] = list3_
+    return dict3_
 
-p = dict_.keys()
-res = list(zip(p, list1_))
-list2_ = []
-for x in res:
-    m = x[0] + x[1]
-    list2_.append(m)
 
-res1 = []
-for el in list2_:
-    sub = el.split(',')
-    res1.append(sub)
-print(res1)
+def total_dict():
+    dict4_ = {}
+    for m, o in dict_add().items():
+        # dict4_ = {}
+        for i, j in reg()[0].items():
+            if m != i:
+                dict4_[i] = j
+            else:
+                dict4_[m] = o
+    return dict4_
 
-with open("phonebook.csv", "w") as f:
 
-    datawriter = csv.writer(f, delimiter=",")
-    datawriter.writerows(res1)
+list5_ = []
+for i, j in total_dict().items():
+    m = (i + ',' + ','.join(j))
+    list5_.append(m.split(','))
+
+
+if __name__ == '__main__':
+    with open("phonebook.csv", "w") as f:
+        datawriter = csv.writer(f, delimiter=",")
+        datawriter.writerows(list5_)
+
+        datawriter.writerows(list5_)
